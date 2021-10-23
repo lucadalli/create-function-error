20776b6c05157a97acd2aef73aa4a91c72dd7837
Luca Dalli <lucadalli@outlook.com>
2021-10-23 22:28:43 +0200

    Simplify further

--- functions/test.ts
+++ functions/test.ts
@@ -1,5 +1,4 @@
-import stream from "stream";
 import { loadImage, createCanvas } from "canvas";
-import Shopify, { IOrder } from "shopify-api-node";
+import Shopify from "shopify-api-node";
 
 console.log(loadImage, createCanvas, Shopify);
