1f507137f9f006922b79620643f95b5c590203bc
Luca Dalli <lucadalli@outlook.com>
2021-10-23 22:13:30 +0200

    init

---
+++ functions/test.ts
@@ -0,0 +1,5 @@
+import stream from "stream";
+import { loadImage, createCanvas } from "canvas";
+import Shopify, { IOrder } from "shopify-api-node";
+
+console.log(loadImage, createCanvas, Shopify);
